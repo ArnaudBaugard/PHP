18eb255bed0a6d82c5f9355947ed3b9e598b0fd5
ArnaudBaugard <35838625+ArnaudBaugard@users.noreply.github.com>
2018-12-04 14:20:39 +0100

    Modification pour cookie

--- index.php
+++ index.php
@@ -1,31 +1,16 @@
-<?php
-
-if (isset($_COOKIE['pseudo'])) {
-	echo 'Bonjour '.$_COOKIE['pseudo'].' !';
-}
-else {
-	echo 'Notre cookie n\'est pas déclaré.';
-}
-?>
 <!DOCTYPE html>
 <html lang="fr">
 
-
-<body id="page-top" class="index">
+	<body id="page-top" class="index">
 
 <?php 
 
 include("includes/haut.inc.php");
 if(isset($_GET['id'])){
-    $modif = $_GET['id'];
-
+    $identifiant = $_GET['id'];
 }
  ?>
  
-    <!-- About Section -->
-
-
-
     <p>
     <?php
 
@@ -33,42 +18,27 @@ if (isset($_COOKIE['pseudo'])) {
 	echo 'Bonjour '.$_COOKIE['pseudo'].' !';
 }
 else {
-	echo 'Notre cookie n\'est pas déclaré.';
+	echo 'Cookie non déclaré';
 }
-
 ?>
-
-
     </p>
-
-
-
 
     <section>
         <div class="container">
-            <div class="row">              
+            <div class="row">    
+				<?php if(isset($_GET['id'])){ ?>
                 <form action="message.php" method="POST">
                     <div class="col-sm-10">  
                         <div class="form-group">
-                            <textarea id="message" name="message" class="form-control" placeholder="">
-                            <?php 
-                            if(isset($_GET['id'])){
+                            <textarea id="message" name="message" class="form-control" placeholder="Message"><?php 
                                 include("includes/connexion.inc.php");
-                                $query="SELECT * FROM messages WHERE id=$modif ";
-                                $stmt=$pdo->query($query);
-                                while($data=$stmt->fetch()){
-                                    echo $data['contenu'];
-                                }
-}else{
- echo "Messages"; 
-                            }
-   
-                            
-                            ?>
-                            
+                                $query = "SELECT * FROM messages WHERE id = '$identifiant'";
+                              $stmt = $pdo->query($query);
+                              while ($donnees = $stmt->fetch()) {
+                                echo $donnees['contenu'];
+                              }		  
+                            ?>               
                             </textarea>
-
-
                             <input type="hidden" id="id" name= "id" value=" <?php if(isset($_GET['id'])){ echo $_GET['id'];}else{echo 0;} ?> "></input>
                         </div>
                     </div>
@@ -77,30 +47,41 @@ else {
                     </div>                        
                 </form>
             </div>
-
+			  <?php
+                }
+                else{
+              ?>
+                <form action="message.php" method="Post">
+                    <div class="col-sm-10">
+                        <div class="form-group">
+                            <textarea id="message" name="message" class="form-control" placeholder="Message"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-sm-2">
+                        <button type="submit" class="btn btn-success btn-lg">Envoyer</button>
+                    </div>
+                </form>
+            </div>
+			
+          <?php } ?>
 
             <div class="row">
-           </br>
-           </br>
-           </br>
-           </br>
-           </br>
-           </br>
             <?php 
 
                 include("includes/connexion.inc.php");
-                $query="SELECT * FROM messages";
+                $query="SELECT * FROM messages ORDER BY date desc";
                 $stmt=$pdo->query($query);
-                while($data=$stmt->fetch()){
-                    
-                    echo "<blockquote>".$data['contenu']."</blockquote><a href='index.php?id=".$data['id']."'>  <button name='d' type='submit' class='btn btn-secondary'>Modifier</button> </a> <a href='includes\supprimer.php?id=".$data['id']."'> <button name='dd' type='submit' class='btn btn-secondary'>Suppresion</button>  </a>";
-                     echo gmdate("Y-m-d H:i:s", $data['date']);
-                     
-
-                }
-               
-            
-            ?>
+                while($donnees = $stmt->fetch()){
+                          ?><blockquote>
+                              <p><?php echo $donnees['contenu'];?></p>
+                              <footer>
+                                <?php echo date('Y-m-d H:i:s', $donnees['date']);?>
+                              </footer>
+							  <a href="index.php?id=<?php echo $donnees['id'] ?>" class="btn btn-success">Modifier</a> <a href="includes/supprimer.php?id=<?php echo $donnees['id'] ?>" class="btn btn-danger"> Supprimer </a>
+                          </blockquote>
+                    <?php
+                      }
+                    ?>
 
 
                 </div>
@@ -110,7 +91,6 @@ else {
 
 <?php include("includes/bas.inc.php"); ?>
     
-
     <!-- jQuery -->
     <script src="vendor/jquery/jquery.min.js"></script>
 
@@ -124,5 +104,4 @@ else {
     <script src="js/freelancer.min.js"></script>
 
 </body>
-
 </html>
