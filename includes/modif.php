5b3011ca7bda6176ff1d77de940ba4592e3cef34
ArnaudBaugard <35838625+ArnaudBaugard@users.noreply.github.com>
2018-12-04 14:00:51 +0100

    Modification et suppression

--- includes/modif.php
+++ includes/modif.php
@@ -1,6 +1,17 @@
 <?php 
 
-include("includes/connexion.inc.php");
-$query="SELECT id FROM messages(contenu, date) VALUES(:contenu, :date)";
+include ("connexion.inc.php");
+	
+	$query="UPDATE messages SET contenu = :contenu, date = :date WHERE id=:id; ";
+
+	$prep = $pdo->prepare($query);
+
+	$prep->bindValue(':contenu',$_POST['message']);
+	$prep->bindValue(':date',time());
+	$prep->bindValue(':id', $_POST['id']);
+
+	$prep->execute();
+
+header("Location:../index.php");
 
 ?>
